b8f8ed01c3936e488527bc77282ae5051225cf40
leorenis <leorenis@gmail.com>
2019-06-14 06:52:22 -0300

    Adjusting to use IIFE. Immediately Invoked Function Expression

--- src/components/ResourceList/index.js
+++ src/components/ResourceList/index.js
@@ -5,15 +5,13 @@ const ResourceList = ({ resource }) => {
   const [resources, setResources] = useState([])
 
   useEffect(() => {
-    fetchResources(resource)
+    (async resource => {
+      const response = await axios.get(
+        `https://jsonplaceholder.typicode.com/${resource}`
+      )
+      setResources(response.data)
+    })(resource)
   }, [resource])
-
-  const fetchResources = async resource => {
-    const response = await axios.get(
-      `https://jsonplaceholder.typicode.com/${resource}`
-    )
-    setResources(response.data)
-  }
 
   return <div>{resources.length}</div>
 }
